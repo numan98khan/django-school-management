6e4b8013f7261f2694d013bda9999bb47cfe8b31
numan98khan <numan98khan@gmail.com>
2020-01-10 16:01:02 +0500

    skeet

--- ARMS/settings.py
+++ ARMS/settings.py
@@ -130,8 +130,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # User Settings
